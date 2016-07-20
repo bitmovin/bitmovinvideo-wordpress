708ff083844a832d41a731b45fcbb871c37d0f42
DevStruh <struger93@gmx.at>
2016-07-20 10:13:20 +0200

    Added encoding libraries to repo.

---
+++ lib/JobConfig.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cwioro
+ * Date: 23.06.15
+ * Time: 10:56
+ */
+
+namespace bitcodin;
+
+/**
+ * Class JobConfig
+ * @package bitcodin
+ */
+class JobConfig
+{
+    /**
+     * @var Input
+     */
+    public $input;
+
+    /**
+     * @var EncodingProfile
+     */
+    public $encodingProfile = null;
+
+    /**
+     * @var Output
+     */
+    public $output  = null;
+
+    /**
+     * @var int
+     */
+    public $duration;
+
+    /**
+     * @var array
+     */
+    public $manifestTypes = array();
+
+    /**
+     * @var AbstractDRMConfig
+     */
+    public $drmConfig = null;
+
+    public $hlsEncryptionConfig = null;
+
+    /**
+     * @var AudioMetaData[]
+     */
+    public $audioMetaData = array();
+
+    /**
+     * @var VideoMetaData[]
+     */
+    public $videoMetaData = array();
+
+    /**
+     * @var string
+     */
+    public $speed = null;
+
+    /**
+     * @var boolean
+     */
+    public $extractClosedCaptions = false;
+
+    /**
+     * @var string
+     */
+    public $location = null;
+
+    /**
+     * @var boolean
+     */
+    public $deinterlace = null;
+
+    /**
+     * @var MergeAudioChannelConfig[]
+     */
+    public $mergeAudioChannelConfigs = null;
+
+    /**
+     * @var integer
+     */
+    public $startTime = null;
+
+    /**
+     * @return string
+     */
+    public function getRequestBody()
+    {
+        if (is_string($this->manifestTypes))
+            $this->manifestTypes = array($this->manifestTypes);
+        $array = [];
+        $array['inputId'] = $this->input->inputId;
+        $array['encodingProfileId'] = $this->encodingProfile->encodingProfileId;
+        $array['manifestTypes'] = $this->manifestTypes;
+        $array['extractClosedCaptions'] = $this->extractClosedCaptions;
+
+        if(!is_null($this->output))
+            $array['outputId'] = $this->output->outputId;
+
+        if($this->duration != null)
+            $array['duration'] = $this->duration;
+
+        if (!is_null($this->speed))
+            $array['speed'] = $this->speed;
+
+        if (!is_null($this->drmConfig))
+            $array['drmConfig'] = $this->drmConfig;
+
+        if(!empty($this->audioMetaData))
+            $array['audioMetaData'] = $this->audioMetaData;
+
+        if(!empty($this->videoMetaData))
+            $array['videoMetaData'] = $this->videoMetaData;
+
+        if (!is_null($this->hlsEncryptionConfig))
+            $array['hlsEncryptionConfig'] = $this->hlsEncryptionConfig;
+
+        if(!is_null($this->location))
+            $array['location'] = $this->location;
+
+        if(!is_null($this->deinterlace))
+            $array['deinterlace'] = $this->deinterlace;
+
+        if(!is_null($this->startTime))
+            $array['startTime'] = $this->startTime;
+
+        return json_encode($array);
+    }
+}
