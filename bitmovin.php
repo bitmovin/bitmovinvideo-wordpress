39c83f7fea978716da17cefbea7b16a5d2b562de
PatrickStru <struger93@gmx.at>
2016-08-19 11:28:05 +0200

    Some minor cosmetic changes.

--- bitmovin.php
+++ bitmovin.php
@@ -1014,7 +1014,7 @@ function bitmovin_plugin_display_encoding()
                     <table id="encoding-profile-video-representation" class="wp-list-table widefat fixed striped">
                         <tr><th colspan="2"><h4>Video Representation</h4></th></tr> 
                         <tr><th>Resolution</th><td><input type="text" id="bitcodin_video_width" name="bitcodin_video_width" size="20"/> X <input type="text" id="bitcodin_video_height" name="bitcodin_video_height" size="20"/></td></tr>
-                        <tr><th>Video Bitrate</th><td><input type="text" id="bitcodin_video_bitrate" name="bitcodin_video_bitrate" size="50" onkeyup="video_bitrate()"/><span id="vbitrate" class="bitrate">kbps</span></td></tr>
+                        <tr><th>Video Bitrate</th><td><input type="text" id="bitcodin_video_bitrate" name="bitcodin_video_bitrate" onkeyup="video_bitrate()"/><span id="vbitrate" class="bitrate">kbps</span></td></tr>
                         <tr><th>Video Codec</th><td><select id="bitcodin_video_codec" name="bitcodin_video_codec">
                             <option value="h264">h264</option>
                             <option value="hevc">hevc</option>
@@ -1023,7 +1023,7 @@ function bitmovin_plugin_display_encoding()
                     
                     <table id="encoding-profile-audio-representation" class="wp-list-table widefat fixed striped">
                         <tr><th colspan="2"><h4>Audio Representation</h4></th></tr>
-                        <tr><th>Audio Bitrate</th><td><input type="text" id="bitcodin_audio_bitrate" name="bitcodin_audio_bitrate" size="50" onkeyup="audio_bitrate()"/><span id="abitrate" class="bitrate">kbps</span></td></tr>
+                        <tr><th>Audio Bitrate</th><td><input type="text" id="bitcodin_audio_bitrate" name="bitcodin_audio_bitrate" onkeyup="audio_bitrate()"/><span id="abitrate" class="bitrate">kbps</span></td></tr>
                         <tr><th>Audio Codec</th><td><select id="bitcodin_audio_codec" name="bitcodin_audio_codec">
                             <option value="aac">aac</option>
                         </select></td></tr>
@@ -1079,7 +1079,7 @@ function bitmovin_plugin_display_create_encoding_profile() {
                 <table id="video-table" class="wp-list-table widefat fixed striped">
                     <tr><th colspan="2"><h4>Video Representation</h4></th></tr> 
                     <tr><th>Resolution</th><td><input type="number" id="create-encoding-video-width" name="create-encoding-video-width" size="20" required/> X <input type="number" id="create-encoding-video-height" name="create-encoding-video-height" size="20" required/></td></tr>
-                    <tr><th>Video Bitrate</th><td><input type="number" id="create-encoding-video-bitrate" name="create-encoding-video-bitrate" size="50" onkeyup="video_bitrate()" required/><span id="vbitrate" class="bitrate">kbps</span></td></tr>
+                    <tr><th>Video Bitrate</th><td><input type="number" id="create-encoding-video-bitrate" name="create-encoding-video-bitrate" size="50" onkeyup="video_bitrate()" required/><span id="v-bitrate" class="bitrate">kbps</span></td></tr>
                     <tr><th>Video Codec</th><td><select id="create-encoding-video-codec" name="create-encoding-video-codec">
                         <option value="h264">H264</option>
                         <option value="hevc">HEVC</option>
@@ -1089,7 +1089,7 @@ function bitmovin_plugin_display_create_encoding_profile() {
                 <br><br>
                 <table id="audio-table" class="wp-list-table widefat fixed striped">
                     <th colspan="2"><h4>Audio Representation</h4></th></tr>
-                    <tr><th>Audio Bitrate</th><td><input type="number" id="create-encoding-audio-bitrate" name="create-encoding-audio-bitrate" size="50" onkeyup="audio_bitrate()" required/><span id="abitrate" class="bitrate">kbps</span></td></tr>
+                    <tr><th>Audio Bitrate</th><td><input type="number" id="create-encoding-audio-bitrate" name="create-encoding-audio-bitrate" size="50" onkeyup="audio_bitrate()" required/><span id="a-bitrate" class="bitrate">kbps</span></td></tr>
                     <tr><th>Audio Codec</th><td><select id="create-encoding-audio-codec" name="create-encoding-audio-codec">
                         <option value="aac">AAC</option>
                     </select></td></tr>
