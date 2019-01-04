4e287d63b4d058b93d9285b418ff4477714e2b70
Gernot Zwantschko <raeumungsdienst@gmx-topmail.de>
2019-01-04 12:47:32 +0100

    getParameter: Fixed parameter parsing before it gets stored in database

--- bitmovin.php
+++ bitmovin.php
@@ -351,10 +351,11 @@ function bitmovin_player_save_configuration($post_id)
     }
 }
 
+
 function getParameter($param)
 {
-    $param = (isset($_POST[$param]) ? $_POST[$param] : '');
-    return strip_tags(json_encode($param));
+    $param = (isset($_POST[$param]) ? $_POST[$param] : ‘’);
+    return json_encode(strip_tags(preg_replace(‘/(\r|\n)/‘, ‘’, $param)));
 }
 
 // Player generation
